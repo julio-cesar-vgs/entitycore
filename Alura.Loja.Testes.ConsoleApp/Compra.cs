02296a10222a418c302d27c0aad41efaa6ed9b23
jcsouza <jcsouza@latam.stefanini.com>
2019-01-15 14:12:59 -0200

    05 - Relacionamentos Um para Muitos no Entity

---
+++ Alura.Loja.Testes.ConsoleApp/Compra.cs
@@ -0,0 +1,17 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class Compra
+    {
+        public int Quantidade { get; internal set; }
+        public int ProdutoId { get; set; }
+        public Produto Produto { get; internal set; }
+        public double Preco { get; internal set; }
+        public int Id { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Compra: {this.Id}, {this.Produto}, {this.Preco}, {this.Quantidade} ";
+        }
+    }
+}
--- Alura.Loja.Testes.ConsoleApp/LojaContext.cs
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -6,6 +6,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     internal class LojaContext : DbContext
     {
      public DbSet<Produto>  Produtos { get; set; }
+        public DbSet<Compra> Compras { get; set; }
 
         /// <summary>
         /// Informa a String de configuração 
